998ac8e8af135a1661322a2ab6d6d9e1e4faa120
Israel <israel.pe@outlook.es>
2025-02-20 12:14:17 -0500

    feat: update ProductsController to handle findOne logic and remove ProductsService dependency

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { Get, Post, Patch, Delete, Inject } from '@nestjs/common';
 import { PRODUCT_SERVICE } from '../config';
 import { ClientProxy } from '@nestjs/microservices';
 import { PaginationDto } from '../common/dtos';
+import { firstValueFrom } from 'rxjs';
 
 @Controller('products')
 export class ProductsController {
@@ -21,8 +28,15 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findOne() {
-    return 'Esto deberia retornar un producto';
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    try {
+      const product = await firstValueFrom(
+        this.productsClient.send({ cmd: 'find_one' }, { id }),
+      );
+      return product;
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 
   @Delete(':id')
